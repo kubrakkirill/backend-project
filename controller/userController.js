268be6ef2a3181b1709c7c03dab7406b33684489
kubrakk <105593907+kubrakkirill@users.noreply.github.com>
2023-03-09 20:20:49 +0100

    add hash password

--- controller/userController.js
+++ controller/userController.js
@@ -1,15 +1,30 @@
 const {request, response} = require("express");
 const userModel = require('../models/userModel.js')
+const bcrypt = require('bcrypt')
 
 
 const logIn = (request, response) => {
     response.render('index')
 }
-const signUp = (request, response) => {
+const signUp = async (request, response) => {
+    let existUser = await userModel.findOne({email: request.body.email})
     let newUser = new userModel(request.body);
     newUser.save()
-        .then(() => {
-            console.log(newUser)
+        .then( async () => {
+            if (existUser){
+                response.render('index',{
+                    error: 'Email is already in use!'
+                })
+            } else {
+                if (!request.body.password){
+                    response.render('index',{
+                        error: 'Password is required'
+                    })
+                } else {
+                    let hashedPassword = await bcrypt.hash(request.body.password, 12)
+                }
+            }
+
             response.render('homePage')
         })
         .catch(error => {
