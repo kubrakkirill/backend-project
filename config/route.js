3ee6d7b4a7cd62cc83bd62668b0dedeaf4b72307
islam altayeb <eslameltaieb74@gmail.com>
2023-03-09 18:06:00 +0100

    log-in function

--- config/route.js
+++ config/route.js
@@ -4,4 +4,7 @@ const userController = require('../controller/userController');
 
 route.get('/', userController.account);
 
+//log-in function
+route.post('/log-in', userController.logIn)
+
 module.exports = route;
--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,5 @@
 const {request, response} = require("express");
+const userModel = require("../models/userModel");
 
 const account = (request, response) => {
     response.render('index',{
@@ -6,6 +7,31 @@ const account = (request, response) => {
     })
 }
 
+const logIn = async (req, res) => {
+    if (!req.body.email || !req.body.password) {
+        res.render('index',{
+            error: 'email and password are required'
+        })
+    }else{
+        let user = await userModel.findOne({email : req.body.email});
+        if (!user) {
+            res.render('index',{
+                error: 'user is not exist, please sign up first!'
+            })
+        }else{
+            let correctPass = await bcrypt.compareSync(req.body.password, user.password)
+            if (!correctPass) {
+                res.render('index',{
+                    error: 'password is not correct'
+            })
+        }else{
+            redirect('/homePage')
+        }
+    }
+  }
+}
+
 module.exports = {
     account,
+    logIn,
 }
