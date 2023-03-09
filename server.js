28bf7d6239a0a2823a31584d7acaa1af07de24e1
kubrakk <105593907+kubrakkirill@users.noreply.github.com>
2023-03-09 15:40:26 +0100

    add log in and sign up forms add '/' to the route.js add logIn function to the userController.js

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+const route = require('./config/route');
+
+app.set('view engine', 'ejs');
+
+app.use(route);
 
 let PORT = 3000;
 app.listen(PORT, () => console.log(`The Timeline is on ${PORT}`));
