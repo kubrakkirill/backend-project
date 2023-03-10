1407ddf276614bec20bd9f6961e818a081d53a98
kubrakk <105593907+kubrakkirill@users.noreply.github.com>
2023-03-10 12:38:02 +0100

    rebuild signUp function, now it has JWT, hash password add logout function in userController.js add logout button at homePage.ejs

--- config/route.js
+++ config/route.js
@@ -3,7 +3,9 @@ const route = express.Router();
 const userController = require('../controller/userController');
 
 route.get('/', userController.logIn);
+route.get('/logOut', userController.logOut);
 
 route.post('/signUp', userController.signUp);
+
 
 module.exports = route;
--- controller/userController.js
+++ controller/userController.js
@@ -1,6 +1,7 @@
 const {request, response} = require("express");
-const userModel = require('../models/userModel.js')
-const bcrypt = require('bcrypt')
+const userModel = require('../models/userModel.js');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 
 const logIn = (request, response) => {
@@ -8,29 +9,42 @@ const logIn = (request, response) => {
 }
 const signUp = async (request, response) => {
     let existUser = await userModel.findOne({email: request.body.email})
-    let newUser = new userModel(request.body);
-    newUser.save()
-        .then( () => {
-            if (existUser){
-                response.render('index',{
-                    error: 'Email is already in use!'
-                })
-            } else {
-                if (!request.body.password){
-                    response.render('index',{
-                        error: 'Password is required'
-                    })
-                }
-            }
+    if (existUser){
+        response.render('index', {
+            error: 'This email is already in use!'
+        })
+    } else {
+        if (!request.body.password){
+            response.render('index', {
+                error: 'Password is require!'
+            })
+        } else {
+            var hashedPassword = await bcrypt.hashSync(request.body.password, 12)
+        }
+        let newUserObj = {
+            ...request.body,
+            password: hashedPassword
+        }
+        let newUser = new userModel(newUserObj)
+        newUser.save()
+            .then ( async () => {
+                let newToken = await jwt.sign({newUser}, 'Token')
+                response.cookie('jwt', newToken, {httpOnly: true})
+                response.render('homePage')
+            })
+            .catch ( error => {
+                throw error
+            })
+    }
+}
 
-            response.render('homePage')
-        })
-        .catch(error => {
-            console.log(error)
-        })
+const logOut = (request, response) => {
+    response.clearCookie('jwt');
+    response.redirect('/')
 }
 
 module.exports = {
     logIn,
-    signUp
+    signUp,
+    logOut
 }
