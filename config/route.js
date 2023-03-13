06051f1e8b8dc5d4e66850c7a6426088122fa096
islam altayeb <eslameltaieb74@gmail.com>
2023-03-13 15:39:32 +0100

    edit route with auth

--- config/route.js
+++ config/route.js
@@ -7,13 +7,13 @@ route.get('/', userController.account);
 
 //log-in function
 
-route.get('/', userController.logIn);
+route.get('/', middleware.loggedInAlready, userController.logIn);
 route.get('/logOut', userController.logOut);
 
 route.post('/homepage', userController.signUp);
 
 //log-in function
-route.post('/log-in', middleware.checkLogIn, userController.logIn)
+route.post('/log-in',middleware.checkLogIn, userController.logIn)
 
 //add new file route functions
 route.get('/addQuestion' , userController.addNew)
