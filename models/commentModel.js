c47853a36bdbc2331facfabedf6dd178de048c0a
kubrakk <105593907+kubrakkirill@users.noreply.github.com>
2023-03-14 17:32:52 +0100

    add moment

--- models/commentModel.js
+++ models/commentModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const moment = require("moment");
 
 const commentSchema = mongoose.Schema({
 
@@ -6,7 +7,14 @@ const commentSchema = mongoose.Schema({
         type : String,
         required : true,
     },
+    created_at: {
+        type: Date,
+        default: Date.now,
+        get: function (createdAt) {
+            return moment(createdAt).format('MMMM Do YYYY, h:mm:ss a');
+        }
+    },
 
-})
+}, {timestamps: true})
 
 module.exports = mongoose.model('comment', commentSchema);
